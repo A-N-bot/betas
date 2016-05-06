842817d4fad7878374cd170ecadf3502b3e31ff3
TELEBETA <MOMO13810114@GMAIL.COM>
2016-05-06 15:52:06 +0430

    Update set.lua

--- plugins/set.lua
+++ plugins/set.lua
@@ -13,7 +13,7 @@ local function save_value(msg, name, value)
 end
 local function run(msg, matches)
   if not is_momod(msg) then
-    return "For moderators only!"
+    return "فقط برای ادمینها مجاز است🔱"
   end
   local name = string.sub(matches[1], 1, 50)
   local value = string.sub(matches[2], 1, 1000)
@@ -25,7 +25,7 @@ end
 
 return {
   patterns = {
-   "^[!/]save ([^%s]+) (.+)$"
+   "^ذخیره ([^%s]+) (.+)$"
   }, 
   run = run 
 }
